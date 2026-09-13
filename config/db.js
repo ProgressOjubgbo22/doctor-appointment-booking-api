add34eaf683ff577ded61000df7e9f810bc3a21a
Ojugbo Progress <lingardj852@gmail.com>
2026-09-13 14:05:03 +0100

    chore: initialize backend

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     logger.info(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    logger.info(`MongoDB connected: ${conn.connection.host}`);
+    logger.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
   }
 };
